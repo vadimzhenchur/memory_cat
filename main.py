9c5c6e6de65570c6a2552ccbebcad4d5887caeaf
vadimzhenchur <141917559+vadimzhenchur@users.noreply.github.com>
2023-08-27 18:27:31 +0300

    memory_cat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import random
 from PyQt5.QtWidgets import *
+import base
 
 app = QApplication([])
 window = QWidget()
@@ -13,7 +15,7 @@ timeSpn = QSpinBox()
 timeLbl = QLabel("хвилин")
 Anwer = QPushButton("Відповісти")
 Anwer1 = QLabel("Яблуко")
-
+Anwer2 = QPushButton("Наступне питання")
 
 firstLine = QHBoxLayout()
 firstLine.addWidget(menuBth)
@@ -32,6 +34,8 @@ answer1 = QRadioButton("1")
 answer2 = QRadioButton("2")
 answer3 = QRadioButton("3")
 answer4 = QRadioButton("4")
+answers = [answer1, answer2, answer3, answer4]
+
 
 answerLine.addWidget(answer1)
 answerLine.addWidget(answer2)
@@ -40,9 +44,40 @@ answerLine.addWidget(answer4)
 answerGroups.setLayout(answerLine)
 mainLine.addWidget(answerGroups)
 
+result = QLabel("Результат")
+answerLine.addWidget(result)
+result.hide()
+
 qwertyLine = QHBoxLayout()
 qwertyLine.addWidget(Anwer)
+qwertyLine.addWidget(Anwer2)
 mainLine.addLayout(qwertyLine)
+Anwer2.hide()
+
+def setQuest():
+    random.shuffle(answers)
+    Anwer1.setText(base.qeust[base.currentQuest]["питання"])
+    answers[3].setText(base.qeust[base.currentQuest]["не правильна1"])
+    answers[1].setText(base.qeust[base.currentQuest]["не правильна2"])
+    answers[2].setText(base.qeust[base.currentQuest]["не правильна3"])
+    answers[0].setText(base.qeust[base.currentQuest]["Правильна відповідь"])
+setQuest()
+def showResult():
+    answers[0].hide()
+    answers[1].hide()
+    answers[2].hide()
+    answers[3].hide()
+    result.show()
+    Anwer2.show()
+    Anwer.hide()
+    if answers[0].isChecked():
+        result.setText("Правильно")
+    else:
+        result.setText("не правильно")
+
+
+
+Anwer.clicked.connect(showResult)
 
 window.setLayout(mainLine)
 window.show()
